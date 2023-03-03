7441fe3b0b8531d8018470c0555e0c02bd349384
sumano1503 <C14190159@john.petra.ac.id>
2023-03-03 23:09:47 +0700

    sepeda

---
+++ config/constant.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+
+func SetUpConfig() *oauth2.Config {
+	conf := &oauth2.Config{
+		ClientID: "490179235093-n8vurjhtci5qp13di65nd1g9qbqoevh7.apps.googleusercontent.com",
+		ClientSecret: "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",
+		RedirectURL: "http://localhost:8080/google/callback",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
+	}
+	return conf
+}
--- controllers/logincontroller/logincontroller.go
+++ controllers/logincontroller/logincontroller.go
@@ -1,29 +1,47 @@
 package logincontroller
 
 import (
+	"context"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
+	"github.com/Sumano1503/petrapitpitanbackend/config"
 )
 
 func GoogleLogin(res http.ResponseWriter, req *http.Request){
-	conf := &oauth2.Config{
-		ClientID: "490179235093-n8vurjhtci5qp13di65nd1g9qbqoevh7.apps.googleusercontent.com",
-		ClientSecret: "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",
-		RedirectURL: "http://localhost:8080/google/callback",
-		Scopes: []string{
-			"https://www.googleapis.com/auth/userinfo.email",
-			"https://www.googleapis.com/auth/userinfo.profile",
-		},
-		Endpoint: google.Endpoint,
-	}
-
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
+ googleConfig := config.SetUpConfig()
+ url := googleConfig.AuthCodeURL("randomstate")
 
  http.Redirect(res, req, url, http.StatusSeeOther)
 }
 
 func GoogleCallback(res http.ResponseWriter, req *http.Request){
+	state := req.URL.Query () ["state" ] [0]
+	if state != "randomstate" {
+		fmt.Fprintln(res,"states dont match ")
+		return
+	}
+
+	code := req.URL.Query()["code"][0]
+
+	googleConfig := config.SetUpConfig()
+
+	token, err := googleConfig.Exchange(context.Background(), code)
+	if err != nil {
+		fmt.Println(res, "CODE exchange failed")
+	}
+
+	resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		fmt.Println(res, "User Data Fetch Failed")
+	}
+
+	userData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(res, "JSON Parse Failed")
+	}
+
+	fmt.Println(res, string(userData))
 
 }
