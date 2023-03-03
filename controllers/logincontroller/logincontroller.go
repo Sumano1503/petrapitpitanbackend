4dc807a3130984a6faf9bf8cae6e9a8a6c226bb5
sumano1503 <C14190159@john.petra.ac.id>
2023-03-03 22:45:05 +0700

    sepeda

--- controllers/logincontroller/logincontroller.go
+++ controllers/logincontroller/logincontroller.go
@@ -3,12 +3,23 @@ package logincontroller
 import (
 	"net/http"
 
-	"github.com/Sumano1503/petrapitpitanbackend/config"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 func GoogleLogin(res http.ResponseWriter, req *http.Request){
- googleConfig := config.SetUpConfig()
- url := googleConfig.AuthCodeURL("randomstate")
+	conf := &oauth2.Config{
+		ClientID: "490179235093-n8vurjhtci5qp13di65nd1g9qbqoevh7.apps.googleusercontent.com",
+		ClientSecret: "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",
+		RedirectURL: "http://localhost:8080/google/callback",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile",
+		},
+		Endpoint: google.Endpoint,
+	}
+
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
 
  http.Redirect(res, req, url, http.StatusSeeOther)
 }
