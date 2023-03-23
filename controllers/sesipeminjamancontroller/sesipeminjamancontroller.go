7fbf1f2eff9caabaa6c9f46d020681b1d6a007dc
sumano1503 <C14190159@john.petra.ac.id>
2023-03-23 21:28:20 +0700

    sepeda

--- controllers/sesipeminjamancontroller/sesipeminjamancontroller.go
+++ controllers/sesipeminjamancontroller/sesipeminjamancontroller.go
@@ -10,8 +10,9 @@ import (
 func GetSesi1Halte1(c *gin.Context) {
 	var sesipeminjaman []models.SesiPeminjaman
 
-	if err := models.DB.Where("sesi = ? AND id_halte = ? AND ", 1, 1).Find(&sesipeminjaman).Error; err != nil {
+	if err := models.DB.Where("sesi = ? AND id_halte = ?", 1, 1).Find(&sesipeminjaman).Error; err != nil {
 		c.AbortWithStatus(http.StatusNotFound)
+		// c.AbortWithStatus(http.StatusNotFound, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": sesipeminjaman})
