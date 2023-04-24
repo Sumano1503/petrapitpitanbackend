01f40f01dd1d74b920cef0422b7cc23e79d5917f
sumano1503 <C14190159@john.petra.ac.id>
2023-04-24 16:53:26 +0700

    sepeda

--- controllers/notificationcontroller/notificationcontroller.go
+++ controllers/notificationcontroller/notificationcontroller.go
@@ -38,105 +38,105 @@ func PushNotification(c *gin.Context){
 		return 
 	}
 
-	if(reqBody.Tipe == 1){
-		payload, err := json.Marshal(map[string]interface{}{
-			"app_id":                  "59865fb1-ab37-4f3a-9f21-e41e33194070",
-			"include_external_user_ids": reqBody.ExternalIDs,
-			"contents":                "Waktu Peminjaman Telah Habis Mohon Segera Mengembalikan Sepeda!!!!",
-			"headings":                "Waktu Peminjaman Telah Habis",
-			"subtitle":                "Anda Akan Dinonaktifkan Jika Tidak Segera Mengembalikan Sepeda",
-		})
+	// if(reqBody.Tipe == 1){
+	// 	payload, err := json.Marshal(map[string]interface{}{
+	// 		"app_id":                  "59865fb1-ab37-4f3a-9f21-e41e33194070",
+	// 		"include_external_user_ids": reqBody.ExternalIDs,
+	// 		"contents":                "Waktu Peminjaman Telah Habis Mohon Segera Mengembalikan Sepeda!!!!",
+	// 		"headings":                "Waktu Peminjaman Telah Habis",
+	// 		"subtitle":                "Anda Akan Dinonaktifkan Jika Tidak Segera Mengembalikan Sepeda",
+	// 	})
 	
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create push notification payload"})
-			return
-		}
+	// 	if err != nil {
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create push notification payload"})
+	// 		return
+	// 	}
 	
-		client := &http.Client{}
-			req, err := http.NewRequest("POST", "https://onesignal.com/api/v1/notifications", bytes.NewBuffer(payload))
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create HTTP request for push notification"})
-				return
-			}
+	// 	client := &http.Client{}
+	// 		req, err := http.NewRequest("POST", "https://onesignal.com/api/v1/notifications", bytes.NewBuffer(payload))
+	// 		if err != nil {
+	// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create HTTP request for push notification"})
+	// 			return
+	// 		}
 	
-		req.Header.Add("Authorization", "Basic ZDFhOGQ4MmYtNzZmMy00Zjg1LTkyYzctNmIyNDI0MGJhNjU0")
-		req.Header.Add("Content-Type", "application/json")
-		req.Header.Add("Accept", "application/json")
+	// 	req.Header.Add("Authorization", "Basic ZDFhOGQ4MmYtNzZmMy00Zjg1LTkyYzctNmIyNDI0MGJhNjU0")
+	// 	req.Header.Add("Content-Type", "application/json")
+	// 	req.Header.Add("Accept", "application/json")
 	
-		resp, err := client.Do(req)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to send push notification client"})
-				return
-			}
-			defer resp.Body.Close()
+	// 	resp, err := client.Do(req)
+	// 		if err != nil {
+	// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to send push notification client"})
+	// 			return
+	// 		}
+	// 		defer resp.Body.Close()
 	
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to read push notification response"})
-			return
-		}
+	// 	body, err := ioutil.ReadAll(resp.Body)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to read push notification response"})
+	// 		return
+	// 	}
 	
-		var respBody map[string]interface{}
-		if err := json.Unmarshal(body, &respBody); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to parse push notification response"})
-			return
-		}
+	// 	var respBody map[string]interface{}
+	// 	if err := json.Unmarshal(body, &respBody); err != nil {
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to parse push notification response"})
+	// 		return
+	// 	}
 	
-		notificationID := respBody["id"].(string)
+	// 	notificationID := respBody["id"].(string)
 	
-		c.JSON(http.StatusOK, gin.H{"message": "push notification sent", "notification_id": notificationID})
-	}else{
-		payload, err := json.Marshal(map[string]interface{}{
-			"app_id":                  "59865fb1-ab37-4f3a-9f21-e41e33194070",
-			"include_external_user_ids": reqBody.ExternalIDs,
-			"contents":                "Harap Segera Kembali Ke Area Bersepeda",
-			"headings":                "Anda Keluar Dari Area Bersepeda!!!!",
-			"subtitle":                "Anda Akan Dinonaktifkan Jika Tidak Segera Kembali Ke Area Bersepeda",
-		})
+	// 	c.JSON(http.StatusOK, gin.H{"message": "push notification sent", "notification_id": notificationID})
+	// }else{
+	// 	payload, err := json.Marshal(map[string]interface{}{
+	// 		"app_id":                  "59865fb1-ab37-4f3a-9f21-e41e33194070",
+	// 		"include_external_user_ids": reqBody.ExternalIDs,
+	// 		"contents":                "Harap Segera Kembali Ke Area Bersepeda",
+	// 		"headings":                "Anda Keluar Dari Area Bersepeda!!!!",
+	// 		"subtitle":                "Anda Akan Dinonaktifkan Jika Tidak Segera Kembali Ke Area Bersepeda",
+	// 	})
 	
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create push notification payload"})
-			return
-		}
+	// 	if err != nil {
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create push notification payload"})
+	// 		return
+	// 	}
 	
-		client := &http.Client{}
-			req, err := http.NewRequest("POST", "https://onesignal.com/api/v1/notifications", bytes.NewBuffer(payload))
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create HTTP request for push notification"})
-				return
-			}
+	// 	client := &http.Client{}
+	// 		req, err := http.NewRequest("POST", "https://onesignal.com/api/v1/notifications", bytes.NewBuffer(payload))
+	// 		if err != nil {
+	// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create HTTP request for push notification"})
+	// 			return
+	// 		}
 	
-		req.Header.Add("Authorization", "Basic ZDFhOGQ4MmYtNzZmMy00Zjg1LTkyYzctNmIyNDI0MGJhNjU0")
-		req.Header.Add("Content-Type", "application/json")
-		req.Header.Add("Accept", "application/json")
+	// 	req.Header.Add("Authorization", "Basic ZDFhOGQ4MmYtNzZmMy00Zjg1LTkyYzctNmIyNDI0MGJhNjU0")
+	// 	req.Header.Add("Content-Type", "application/json")
+	// 	req.Header.Add("Accept", "application/json")
 	
-		resp, err := client.Do(req)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to send push notification client"})
-				return
-			}
-			defer resp.Body.Close()
+	// 	resp, err := client.Do(req)
+	// 		if err != nil {
+	// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to send push notification client"})
+	// 			return
+	// 		}
+	// 		defer resp.Body.Close()
 	
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to read push notification response"})
-			return
-		}
+	// 	body, err := ioutil.ReadAll(resp.Body)
+	// 	if err != nil {
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to read push notification response"})
+	// 		return
+	// 	}
 	
-		var respBody map[string]interface{}
-		if err := json.Unmarshal(body, &respBody); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to parse push notification response"})
-			return
-		}
+	// 	var respBody map[string]interface{}
+	// 	if err := json.Unmarshal(body, &respBody); err != nil {
+	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to parse push notification response"})
+	// 		return
+	// 	}
 	
-		notificationID := respBody["id"].(string)
+	// 	notificationID := respBody["id"].(string)
 	
-		c.JSON(http.StatusOK, gin.H{"message": "push notification sent", "notification_id": notificationID})
-	}
+	// 	c.JSON(http.StatusOK, gin.H{"message": "push notification sent", "notification_id": notificationID})
+	// }
 
 	payload, err := json.Marshal(map[string]interface{}{
 		"app_id":                  "59865fb1-ab37-4f3a-9f21-e41e33194070",
-		"include_external_user_ids": AdminId,
+		"include_external_user_ids": reqBody.ExternalIDs,
 		"contents":                "Telah terjadi pelanggaran yang dilakukan peminjam",
 		"headings":                "Pelanggaran!!!!!",
 		"subtitle":                "Mohon cek detail pelangaran",
