daa3bbd24c4f1de3f314c82ccde91a99cae9fa2d
sumano1503 <C14190159@john.petra.ac.id>
2023-05-08 15:44:24 +0700

    sesi peminjaman add

--- controllers/detailsepedahaltecontroller/detailsepedahaltecontroller.go
+++ controllers/detailsepedahaltecontroller/detailsepedahaltecontroller.go
@@ -129,7 +129,7 @@ func DeleteByIdSepeda(c *gin.Context) {
 func GetSepedaHalte1(c *gin.Context) {
 	var detailSepedaHalte []models.DetailSepedaHalte;
 	var sepeda []models.Sepeda;
-	var sepedaHalte1 []models.Sepeda;
+	var sepedaHalte []models.Sepeda;
 	models.DB.Find(&detailSepedaHalte)
 		// c.JSON(http.StatusOK, gin.H{"detailSepedaHalte": detailSepedaHalte})
 	models.DB.Find(&sepeda)
@@ -138,13 +138,13 @@ func GetSepedaHalte1(c *gin.Context) {
 	for i:= 0; i < len(detailSepedaHalte); i++ {
 		for j:= 0; j < len(sepeda); j++ {
 			if detailSepedaHalte[i].Id_sepeda == sepeda[j].Id {
-				sepedaHalte1 = append(sepedaHalte1, sepeda[j])
+				sepedaHalte = append(sepedaHalte, sepeda[j])
 			}
 		}
 	}
 	
 	
-	c.JSON(http.StatusOK, gin.H{"sepedaHalte1": sepedaHalte1})
+	c.JSON(http.StatusOK, gin.H{"sepeda": sepedaHalte})
 }
 
 func GetSepedaHalte2(c *gin.Context) {
