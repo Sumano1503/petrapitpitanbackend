b05cd416bb148c538cddc6c23f24f6cd4ee0b577
sumano1503 <C14190159@john.petra.ac.id>
2023-05-09 23:02:11 +0700

    nambah polygon

--- controllers/polygoncontroller/polygoncontroller.go
+++ controllers/polygoncontroller/polygoncontroller.go
@@ -16,19 +16,20 @@ func Index(c *gin.Context){
 
 func Create(c *gin.Context) {
 	var polygoninput models.Polygon
-	// var polygon models.Polygon
-	
 
 	if err := c.ShouldBindJSON(&polygoninput); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return 
 	}
-	
-	if models.DB.Model(&polygoninput).Where("id = ?", polygoninput.Id).Updates(&polygoninput).RowsAffected == 0 {
-		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "TIDAK DAPAT MENGUPDATE"})
-		return 
+
+	result := models.DB.Table("polygons").Where("id = ? ", polygoninput.Id)
+	if result.Error != nil{
+		if models.DB.Model(&polygoninput).Where("id = ?", polygoninput.Id).Updates(&polygoninput).RowsAffected == 0 {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "TIDAK DAPAT MENGUPDATE"})
+			return 
+		}
+	}else{
+		models.DB.Create(&polygoninput);
 	}
-	
 	c.JSON(http.StatusOK, gin.H{"polygon": polygoninput})
-	
 }
