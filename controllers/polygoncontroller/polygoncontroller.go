26e7cf849ba8e8289a33e6e05e87cd5002820280
sumano1503 <C14190159@john.petra.ac.id>
2023-05-10 12:42:51 +0700

    nambah polygon

--- controllers/polygoncontroller/polygoncontroller.go
+++ controllers/polygoncontroller/polygoncontroller.go
@@ -5,12 +5,31 @@ import (
 
 	"github.com/Sumano1503/petrapitpitanbackend/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func Index(c *gin.Context){
 	var polygon []models.Polygon
 
 	models.DB.Find(&polygon)
+	c.JSON(http.StatusOK, gin.H{"polygon": polygon})
+}
+
+func Show(c *gin.Context){
+	var polygon models.Polygon
+
+	id := c.Param("id")
+
+	if err := models.DB.First(&polygon, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Record not found!"})
+			return 
+		default:
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+	}
+
 	c.JSON(http.StatusOK, gin.H{"polygon": polygon})
 }
 
