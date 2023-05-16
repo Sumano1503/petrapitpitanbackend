259479a2abf1063fc6fd211ba9f8d9d3cfa74c65
sumano1503 <C14190159@john.petra.ac.id>
2023-05-16 18:55:18 +0700

    nambah polygon

--- controllers/detailpeminjamancontroller/detailpeminjamancontroller.go
+++ controllers/detailpeminjamancontroller/detailpeminjamancontroller.go
@@ -145,17 +145,15 @@ func HistoryUser(c *gin.Context){
 	var user models.User
 
 	email := c.Param("email")
-	if err := models.DB.Where("email = ?", email).First(&user).Error; err != nil {
+	if err := models.DB.Where("email = ?", email).Find(&user).Error; err != nil {
 		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Record not found!"})
 	}
-
 	id := user.Id
-
 	if err := models.DB.Where("id_user = ? AND status = 'done' OR  status = 'Di Batalkan'", id).Find(&detailPeminjaman).Error; err != nil {
 		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Record not found!"})
 	}
 
-	c.JSON(http.StatusOK, gin.H{"data": detailPeminjaman})
+	c.JSON(http.StatusOK, gin.H{"data": detailPeminjaman, "id": user.Id, "email": email, "user": user })
 }
 
 func DetailHistoryUser(c *gin.Context){
