ee1b27bdd836427bc2c13ab1d50a825dc1a77c16
sumano1503 <C14190159@john.petra.ac.id>
2023-06-27 15:43:56 +0700

    revisi

--- controllers/laporancontroller/laporancontroller.go
+++ controllers/laporancontroller/laporancontroller.go
@@ -1,16 +1,20 @@
 package laporancontroller
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Sumano1503/petrapitpitanbackend/models"
 	"github.com/gin-gonic/gin"
 )
 
 func GetLaporan(c *gin.Context){
 	type tanggal struct{
-		Start string
+		Start string 
 		End string
 	}
+
+	var detail_peminjamen []models.DetailPeminjaman
 
 	var date tanggal
 
@@ -18,7 +22,15 @@ func GetLaporan(c *gin.Context){
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return 
 	}
+
+	query := fmt.Sprintf("SELECT * FROM detail_peminjaman WHERE STR_TO_DATE(tanggal, '%%d/%%m/%%Y') BETWEEN STR_TO_DATE('%s', '%%d/%%m/%%Y') AND STR_TO_DATE('%s', '%%d/%%m/%%Y')", date.Start, date.End)
+
+	// Menjalankan query dan mendapatkan hasil
+	if err := models.DB.Raw(query).Scan(&detail_peminjamen).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Record not found!"})
+		return
+	}
 	
 	
-	c.JSON(http.StatusOK, gin.H{"data": date})
+	c.JSON(http.StatusOK, gin.H{"data": detail_peminjamen})
 }
