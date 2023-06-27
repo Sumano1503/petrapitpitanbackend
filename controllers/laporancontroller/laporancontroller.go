141fd9914d0e32839517d86564083aadc89de7ae
sumano1503 <C14190159@john.petra.ac.id>
2023-06-27 16:17:18 +0700

    revisi

--- controllers/laporancontroller/laporancontroller.go
+++ controllers/laporancontroller/laporancontroller.go
@@ -3,34 +3,83 @@ package laporancontroller
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/Sumano1503/petrapitpitanbackend/models"
 	"github.com/gin-gonic/gin"
 )
 
 func GetLaporan(c *gin.Context){
+	type laporan struct{
+		BanyakPeminjaman int
+		BanyakPelanggaran int
+		SepedaBaru int
+		HalteBaru int
+		TotalSepeda int
+		SepedaRusak int
+		detailpeminjaman []models.DetailPeminjaman
+	}
+
 	type tanggal struct{
 		Start string 
 		End string
 	}
 
 	var detail_peminjamen []models.DetailPeminjaman
-
+	var pelanggaran []models.Pelanggaran
+	var sepeda []models.Sepeda
 	var date tanggal
+	var listLaporan laporan
 
 	if err := c.ShouldBindJSON(&date); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return 
 	}
 
-	query := fmt.Sprintf("SELECT * FROM detail_peminjamen WHERE STR_TO_DATE(tanggal, '%%d/%%m/%%Y') BETWEEN STR_TO_DATE('%s', '%%d/%%m/%%Y') AND STR_TO_DATE('%s', '%%d/%%m/%%Y')", date.Start, date.End)
-
-	// Menjalankan query dan mendapatkan hasil
-	if err := models.DB.Raw(query).Scan(&detail_peminjamen).Error; err != nil {
+	// getdetailpeminjaman
+	queryDetailPeminjaman := fmt.Sprintf("SELECT * FROM detail_peminjamen WHERE STR_TO_DATE(tanggal, '%%d/%%m/%%Y') BETWEEN STR_TO_DATE('%s', '%%d/%%m/%%Y') AND STR_TO_DATE('%s', '%%d/%%m/%%Y')", date.Start, date.End)
+	if err := models.DB.Raw(queryDetailPeminjaman).Scan(&detail_peminjamen).Error; err != nil {
 		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Record not found!"})
 		return
 	}
+
+	// getpelanggaran
+	queryPelanggaran := fmt.Sprintf("SELECT * FROM pelanggarans WHERE STR_TO_DATE(tanggal, '%%d/%%m/%%Y') BETWEEN STR_TO_DATE('%s', '%%d/%%m/%%Y') AND STR_TO_DATE('%s', '%%d/%%m/%%Y')", date.Start, date.End)
+	if err := models.DB.Raw(queryPelanggaran).Scan(&pelanggaran).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Record not found!"})
+		return
+	}
+	// getsepeda
+	models.DB.Find(&sepeda)
+
+	layout := "02/01/2006"
+	start, _ := time.Parse(layout, date.Start)
+	end, _ := time.Parse(layout, date.End)
+	coB:=0
+	coR:=0
+	for i := 0; i < len(sepeda); i++ {
+		tanggal, _ := time.Parse(layout, sepeda[i].Tanggal)
+
+		if tanggal.After(start) && tanggal.Before(end) {
+			coB++
+		}
+
+		if(sepeda[i].Status == 1){
+			coR++;
+		}
+	}
+
+	listLaporan.BanyakPeminjaman = len(detail_peminjamen)
+	listLaporan.BanyakPelanggaran = len(pelanggaran)
+	listLaporan.SepedaBaru = coB
+	listLaporan.SepedaRusak = coR
+	listLaporan.TotalSepeda = len(sepeda)
+	listLaporan.detailpeminjaman = detail_peminjamen
+
+
+	
+
 	
 	
-	c.JSON(http.StatusOK, gin.H{"data": detail_peminjamen})
+	c.JSON(http.StatusOK, gin.H{"data": listLaporan})
 }
